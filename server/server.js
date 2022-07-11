2462cf3b45013199c617558db55bec3be7e457cb
Luke Veitch <63455910+lukeveitch@users.noreply.github.com>
2022-07-11 16:39:51 +0100

    refactors functions

--- server/server.js
+++ server/server.js
@@ -1,57 +1,71 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-
 const SpotifyWebApi = require("spotify-web-api-node")
 
 const app = express()
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post("/refresh", (req, res) => {
-  const refreshToken = req.body.refreshToken
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http://localhost:3000',
-    clientId: '7b119c2c66624c96b22daa923ed53848',
-    clientSecret: '47df5a893cf842edae003eabd2cf72a3',
-    refreshToken,
-  })
+// Create api endpoints
+app.post("/refresh", refreshSpotifyToken())
 
-  spotifyApi
-    .refreshAccessToken()
-    .then(data => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
-
-app.post("/login", (req, res) => {
-  const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: process.env.REDIRECT_URI,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-  })
-
-  spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      })
-    })
-    .catch(err => {
-      res.sendStatus(400)
-    })
-})
+app.post("/login", loginUser())
 
 app.listen(3001)
+
+
+
+
+function loginUser() {
+  return (req, res) => {
+    const code = req.body.code
+
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    })
+
+    spotifyApi
+      .authorizationCodeGrant(code)
+      .then(data => {
+        res.json({
+          accessToken: data.body.access_token,
+          refreshToken: data.body.refresh_token,
+          expiresIn: data.body.expires_in,
+        })
+      })
+      .catch(err => {
+        res.sendStatus(400)
+      })
+  }
+}
+
+function refreshSpotifyToken() {
+  return (req, res) => {
+    const refreshToken = req.body.refreshToken
+
+    const spotifyApi = new SpotifyWebApi({
+      redirectUri: process.env.REDIRECT_URI,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken,
+    })
+
+    spotifyApi
+      .refreshAccessToken()
+      .then(data => {
+        res.json({
+          accessToken: data.body.accessToken,
+          expiresIn: data.body.expiresIn,
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(400)
+      })
+  }
+}
